2acccbf874cd94ede22ce79254558a9dd0566b6e
theekshana16 <60084492+theekshana16@users.noreply.github.com>
2021-03-28 17:17:30 -0700

    Initial commit

---
+++ app/src/main/java/com/example/a123testing/MainActivity.java
@@ -0,0 +1,87 @@
+package com.example.a123testing;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+import android.database.Cursor;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    EditText from, to, price;
+    Button insert, update, delete, view;
+    DBHelper DB;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        from = findViewById(R.id.from);
+        to = findViewById(R.id.to);
+        price = findViewById(R.id.price);
+        insert = findViewById(R.id.btnInsert);
+        update = findViewById(R.id.btnUpdate);
+        delete = findViewById(R.id.btnDelete);
+        view = findViewById(R.id.btnView);
+        DB = new DBHelper(this);
+        insert.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String fromTXT = from.getText().toString();
+                String toTXT = to.getText().toString();
+                String priceTXT = price.getText().toString();
+
+                Boolean checkinsertdata = DB.insertuserdata(fromTXT, toTXT, priceTXT);
+                if(checkinsertdata==true)
+                    Toast.makeText(MainActivity.this, "New Entry Inserted", Toast.LENGTH_SHORT).show();
+                else
+                    Toast.makeText(MainActivity.this, "New Entry Not Inserted", Toast.LENGTH_SHORT).show();
+            }        });
+        update.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String fromTXT = from.getText().toString();
+                String toTXT = to.getText().toString();
+                String priceTXT = price.getText().toString();
+
+                Boolean checkupdatedata = DB.updateuserdata(fromTXT, toTXT, priceTXT);
+                if(checkupdatedata==true)
+                    Toast.makeText(MainActivity.this, "Entry Updated", Toast.LENGTH_SHORT).show();
+                else
+                    Toast.makeText(MainActivity.this, "New Entry Not Updated", Toast.LENGTH_SHORT).show();
+            }        });
+        delete.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String fromTXT = from.getText().toString();
+                Boolean checkudeletedata = DB.deletedata(fromTXT);
+                if(checkudeletedata==true)
+                    Toast.makeText(MainActivity.this, "Entry Deleted", Toast.LENGTH_SHORT).show();
+                else
+                    Toast.makeText(MainActivity.this, "Entry Not Deleted", Toast.LENGTH_SHORT).show();
+            }        });
+
+        view.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Cursor res = DB.getdata();
+                if(res.getCount()==0){
+                    Toast.makeText(MainActivity.this, "No Entry Exists", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                StringBuffer buffer = new StringBuffer();
+                while(res.moveToNext()){
+                    buffer.append("From :"+res.getString(0)+"\n");
+                    buffer.append("To:"+res.getString(1)+"\n");
+                    buffer.append("Price :"+res.getString(2)+"\n\n");
+                }
+
+                AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+                builder.setCancelable(true);
+                builder.setTitle("ALL ENTERED TICKETS");
+                builder.setMessage(buffer.toString());
+                builder.show();
+            }        });
+    }}
